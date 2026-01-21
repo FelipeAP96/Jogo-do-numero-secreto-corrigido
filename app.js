8e5bc4749b4242b583b8314d640a3d657df7cf9a
FelipeAP96 <felipeapereira.digital@gmail.com>
2026-01-21 20:09:29 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 1000;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -64,4 +64,5 @@ function reiniciarJogo() {
     tentativas = 1;
     exibirMensagemInicial();
     document.getElementById('reiniciar').setAttribute('disabled',true);
+
 }
